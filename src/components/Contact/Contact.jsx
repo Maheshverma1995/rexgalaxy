2380581158015de29e15465840f70bf2976a5ca7
abhisheksinghabsk <abhisheksingh8604211348@gmail.com>
2023-07-24 08:38:39 +0530

    header

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import '../../App.css'
-import { AiFillGoogleCircle } from "react-icons/ai";
+import React from "react";
+import "../../App.css";
+import { BsFacebook, BsGoogle, BsYoutube } from "react-icons/bs";
 const Contact = () => {
   return (
     <div>
@@ -13,43 +13,67 @@ const Contact = () => {
         <div className="row">
           <div className="col-lg-6">
             <h3>WE'RE READY, LET'S TALK.</h3>
-            <div class="mb-3">
-              <input type="name" class="form-control" placeholder="Enter Your Name" />
+            <div className="mb-3">
+              <input
+                type="name"
+                className="form-control"
+                placeholder="Enter Your Name"
+              />
             </div>
-            <div class="mb-3">
-              <input type="email" class="form-control" placeholder="Enter Your E-mail" />
+            <div className="mb-3">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter Your E-mail"
+              />
             </div>
-            <div class="mb-3">
-              <textarea class="form-control" id="exampleFormControlTextarea1" placeholder='Message' rows="3"></textarea>
+            <div className="mb-3">
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                placeholder="Message"
+                rows="3"
+              ></textarea>
             </div>
-            <div class="w-50 mb-3 ms-0">
-              <label for="exampleFormControlInput1" class="form-label">Phone</label>
-              <input type="number" class="form-control" />
+            <div className="w-50 mb-3 ms-0">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Phone
+              </label>
+              <input type="number" className="form-control" />
             </div>
           </div>
           <div className="col-lg-6">
             <h3>CONTACT INFO</h3>
             <h5>Address</h5>
-            <p>A 40, Ithum Tower A, 6th Floor, 606 ,Noida Sector 62 Uttar Pradesh, 201301</p>
+            <p>
+              A 40, Ithum Tower A, 6th Floor, 606 ,Noida Sector 62 Uttar
+              Pradesh, 201301
+            </p>
             <h5>Email Us</h5>
             <p>contact@rexgalaxy.com</p>
             <h5>Call us</h5>
             <p>+917411211148</p>
             <h5>Follow Us</h5>
-           <div className="icons">
-            <div className='row'>
-              <div className='col-12'>
-              <AiFillGoogleCircle className='icon-contact' />
-            <AiFillGoogleCircle className='icon-contact' />
-            <AiFillGoogleCircle className='icon-contact' />
+            <div className="icons">
+              <div className="row">
+                <article>
+                  <div>
+                    <BsGoogle className="icon-contact" />
+                  </div>
+                  <div>
+                    <BsYoutube className="icon-contact" />
+                  </div>
+                  <div>
+                    <BsFacebook className="icon-contact" />
+                  </div>
+                </article>
               </div>
             </div>
-           </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/NAV-BAR/HeaderNavBar.jsx
+++ src/components/NAV-BAR/HeaderNavBar.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
-import '../../App.css'
-import { FaFacebook ,FaTwitter ,FaLinkedinIn,FaDribbble,FaInstagram } from "react-icons/fa";
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import "../../App.css";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaLinkedinIn,
+  FaInstagram,
+} from "react-icons/fa";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 const HeaderNavBar = () => {
   return (
     <div>
-      <Navbar className="bg-body-dark header-navbar">
-      <Container>
-        <Navbar.Brand href="#home">+91-7411211148 
-        </Navbar.Brand>
-        <label htmlFor="">contact@rexgalaxy.com</label>
-        <label htmlFor="">A 40, Ithum Noida 62</label>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            <FaFacebook/>
-            <FaTwitter/>
-            <FaLinkedinIn/>
-            <FaDribbble/>
-            <FaInstagram/>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar className="bg-success text-color-white header-navbar">
+        <Container>
+          <Navbar.Brand href="#home">+91-7411211148</Navbar.Brand>
+          <label className="me-5 " htmlFor="">
+            contact@rexgalaxy.com
+          </label>
+          <label htmlFor="">A 40, Ithum Noida 62</label>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text className="m-1">
+              <FaFacebook className="m-3"/>
+              <FaTwitter className="m-3"/>
+              <FaLinkedinIn className="m-3"/>
+              <FaInstagram className="m-3"/>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderNavBar
+export default HeaderNavBar;
